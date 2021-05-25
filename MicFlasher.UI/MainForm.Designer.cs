0d079d1fd590bf5acbbb137f7c6d9b2c472eeda8
Uwe Baumann <uwe.baumann@hotmail.de>
2021-05-25 07:25:41 +0200

    Adjusted display blocks

--- MicFlasher.UI/MainForm.Designer.cs
+++ MicFlasher.UI/MainForm.Designer.cs
@@ -46,12 +46,12 @@ namespace MicFlasher.UI
             // VoiceMeeterLabel
             // 
             this.VoiceMeeterLabel.AutoSize = true;
-            this.VoiceMeeterLabel.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.VoiceMeeterLabel.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.VoiceMeeterLabel.ForeColor = System.Drawing.Color.White;
-            this.VoiceMeeterLabel.Location = new System.Drawing.Point(37, 44);
+            this.VoiceMeeterLabel.Location = new System.Drawing.Point(20, 28);
             this.VoiceMeeterLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.VoiceMeeterLabel.Name = "VoiceMeeterLabel";
-            this.VoiceMeeterLabel.Size = new System.Drawing.Size(69, 45);
+            this.VoiceMeeterLabel.Size = new System.Drawing.Size(64, 41);
             this.VoiceMeeterLabel.TabIndex = 3;
             this.VoiceMeeterLabel.Text = "VM";
             this.VoiceMeeterLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -69,27 +69,27 @@ namespace MicFlasher.UI
             this.VoiceMeeterPanel.Controls.Add(this.VoiceMeeterLabel);
             this.VoiceMeeterPanel.Location = new System.Drawing.Point(0, 0);
             this.VoiceMeeterPanel.Name = "VoiceMeeterPanel";
-            this.VoiceMeeterPanel.Size = new System.Drawing.Size(140, 140);
+            this.VoiceMeeterPanel.Size = new System.Drawing.Size(100, 100);
             this.VoiceMeeterPanel.TabIndex = 4;
             // 
             // MicPanel
             // 
             this.MicPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.MicPanel.Controls.Add(this.MicLabel);
-            this.MicPanel.Location = new System.Drawing.Point(142, 0);
+            this.MicPanel.Location = new System.Drawing.Point(102, 0);
             this.MicPanel.Name = "MicPanel";
-            this.MicPanel.Size = new System.Drawing.Size(140, 140);
+            this.MicPanel.Size = new System.Drawing.Size(100, 100);
             this.MicPanel.TabIndex = 5;
             // 
             // MicLabel
             // 
             this.MicLabel.AutoSize = true;
-            this.MicLabel.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MicLabel.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.MicLabel.ForeColor = System.Drawing.Color.White;
-            this.MicLabel.Location = new System.Drawing.Point(36, 44);
+            this.MicLabel.Location = new System.Drawing.Point(19, 28);
             this.MicLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.MicLabel.Name = "MicLabel";
-            this.MicLabel.Size = new System.Drawing.Size(72, 45);
+            this.MicLabel.Size = new System.Drawing.Size(66, 41);
             this.MicLabel.TabIndex = 3;
             this.MicLabel.Text = "Mic";
             this.MicLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -97,12 +97,12 @@ namespace MicFlasher.UI
             // LightLabel
             // 
             this.LightLabel.AutoSize = true;
-            this.LightLabel.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LightLabel.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.LightLabel.ForeColor = System.Drawing.Color.White;
-            this.LightLabel.Location = new System.Drawing.Point(26, 44);
+            this.LightLabel.Location = new System.Drawing.Point(10, 28);
             this.LightLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.LightLabel.Name = "LightLabel";
-            this.LightLabel.Size = new System.Drawing.Size(91, 45);
+            this.LightLabel.Size = new System.Drawing.Size(84, 41);
             this.LightLabel.TabIndex = 3;
             this.LightLabel.Text = "Light";
             this.LightLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -111,16 +111,16 @@ namespace MicFlasher.UI
             // 
             this.LightPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.LightPanel.Controls.Add(this.LightLabel);
-            this.LightPanel.Location = new System.Drawing.Point(284, 0);
+            this.LightPanel.Location = new System.Drawing.Point(204, 0);
             this.LightPanel.Name = "LightPanel";
-            this.LightPanel.Size = new System.Drawing.Size(140, 140);
+            this.LightPanel.Size = new System.Drawing.Size(100, 100);
             this.LightPanel.TabIndex = 6;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 45F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(424, 140);
+            this.ClientSize = new System.Drawing.Size(304, 99);
             this.Controls.Add(this.LightPanel);
             this.Controls.Add(this.MicPanel);
             this.Controls.Add(this.VoiceMeeterPanel);
